9c61cabe59bfa512b039dd68e95ba8b34fbc027e
Adazhii <adajii@gmail.com>
2023-03-08 11:02:31 +0200

    Lint corrections.

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,20 +1,21 @@
 """
-Sample test
+Sample tests
 """
 from django.test import SimpleTestCase
 
 from app import calc
 
-class CalcTest(SimpleTestCase):
+
+class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
 
-    def test_subtruct_numbers(self):
+    def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
